9697a193e49a16d60aad023467375a5c20a3aa97
ATSU <mifutsua@gmail.com>
2023-11-30 07:24:44 +0900

    update

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -27,13 +27,13 @@ type Command = {
 }[];
 
 export const commands: Command = [
-  { cmd: "about", desc: "about Sat Naing", tab: 8 },
+  { cmd: "about", desc: "about Atsu Suzuki", tab: 8 },
   { cmd: "clear", desc: "clear the terminal", tab: 8 },
   { cmd: "echo", desc: "print out anything", tab: 9 },
   { cmd: "email", desc: "send an email to me", tab: 8 },
   { cmd: "help", desc: "check available commands", tab: 9 },
   { cmd: "history", desc: "view command history", tab: 6 },
-  { cmd: "projects", desc: "view projects that I've coded", tab: 5 },
+  { cmd: "projects", desc: "view projects", tab: 5 },
   { cmd: "pwd", desc: "print current working directory", tab: 10 },
   { cmd: "socials", desc: "check out my social accounts", tab: 6 },
   { cmd: "themes", desc: "check available themes", tab: 7 },
